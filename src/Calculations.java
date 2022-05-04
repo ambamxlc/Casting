119821b6b57a3d91adda908f9b615cc2ab85ed0a
ambamxlc <amber.cruz5@gmail.com>
2022-05-04 14:59:42 -0400

    ccc

---
+++ src/Calculations.java
@@ -0,0 +1,30 @@
+// import java.util.Scanner;
+
+// public class Calculations {
+//     public static void main(String[] args) throws Exception {
+//         Scanner scanner = new Scanner(System.in);
+//         Integer num1 = 5;
+//         Integer num2 = 2;
+//         System.out.println("Enter a number: " + num1);
+//         System.out.println("Enter a number: " + num2);
+
+//         Integer addedNums = num1 + num2;
+//         System.out.println(num1 + " + " + num2 + " = " + addedNums);
+
+//         Integer subNums = num1 - num2;
+//         System.out.println(num1 + " - " + num2 + " = " + subNums);
+
+//         Integer multNums = num1 * num2;
+//         System.out.println(num1 + " * " + num2 + " = " + multNums);
+
+//         double divNums = (float)num1/num2;
+//         System.out.println(num1 + " / " + num2 + " = " + divNums);
+
+//         int modulsNums = num1 % num2;
+//         System.out.println(num1 + " % " + num2 +" = " + modulsNums);
+
+//     }
+// }
+
+
+
---
+++ src/Casting.java
@@ -0,0 +1,16 @@
+// public class Casting {
+//     public static void main(String[] args) {
+//             double num = 112.35;
+//             System.out.println("Double: " + num);
+
+//             int numInt = (int)num;
+//             System.out.println("int: " + numInt);
+
+//             String info = "49";
+//             System.out.println("The string value is: "+ info);
+
+//             int digit = Integer.parseInt(info);
+//             System.out.println("The integer value is: " + digit);
+//     }
+
+// }
